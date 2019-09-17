8ec7968634b74fd3b530a908344a7c8553f7bb38
adityak25 <adityak25@users.noreply.github.com>
2019-09-17 00:35:25 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='SideBar'>
+        <div className='SideBar-content'>
+          <a href='/' className='SideBar-title'>
+            Aditya Kankanala
+          </a>
+          <h1 className='SideBar-title__tagline'>
+            Front End Developer @ Retail Pro
+          </h1>
+          <div className='SideBar-menu'>
+            <div className='SideBar-menu__wrap'>
+              <ul className='SideBar-menu__list'>
+                <li className='SideBar-menu__list__item'>
+                  <a href='/' className='SideBar-menu__list__item__link'>
+                    About
+                  </a>
+                </li>
+
+                <li className='SideBar-menu__list__item'>
+                  <a href='/blog/' className='SideBar-menu__list__item__link'>
+                    Projects
+                  </a>
+                </li>
+
+                <li className='SideBar-menu__list__item'>
+                  <a
+                    href='/about'
+                    className='SideBar-menu__list__item__link active-link'>
+                    Timeline
+                  </a>
+                </li>
+
+                <li className='SideBar-menu__list__item'>
+                  <a href='/contact' className='menu__list__item__link'>
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='MainPage'>
+        <div className='MainPage-content'> Hello Aditya!! </div>
+      </div>
     </div>
   );
 }
